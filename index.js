dc4eaf56d67e88b86620a802e529c1450bc8a9b9
Adrian Mørbán <46272302+adrianmorban@users.noreply.github.com>
2024-09-20 17:58:20 -0400

    Add .gitignore and package.json files, and implement retrieveSession function
    - Added .gitignore file to exclude node_modules directory from version control
    - Added package.json file with dependencies for AWS SDK
    - Implemented retrieveSession function in index.js to retrieve a session from DynamoDB
    - Added deploy.yml workflow file for deploying to AWS Lambda

---
+++ index.js
@@ -0,0 +1,23 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { GetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({ 
+    region: "us-east-1",
+});
+
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const retrieveSession = async (event) => {
+    const { sessionID } = event;
+    sessionID = sessionID.toString();
+    const command = new GetCommand({
+        TableName: "sallySessions",
+        Key: {
+            sessionID: sessionID,
+        }
+    });
+    const response = await ddbDocClient.send(command);
+    return response.Item;
+}
+
+export { retrieveSession }
